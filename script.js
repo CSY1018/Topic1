9ef435322a11f692c5a7e0acf40f914b970e7698
Tom Butler <tom@r.je>
2017-01-15 19:51:48 +0000

    h1 element is updated when page loads

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function print5() {
-        console.log(1);
-        console.log(2);
-        console.log(3);
-        console.log(4);
-        console.log(5);
+function myLoadFunction() {
+        var element = document.getElementById('pageheading');
+        element.firstChild.nodeValue = 'New Heading';
+
+        var element = document.getElementById('paragraph');
+        element.firstChild.nodeValue = 'New paragraph text';
 }
 
 
-print5();
-print5();
+document.addEventListener('DOMContentLoaded', myLoadFunction);
